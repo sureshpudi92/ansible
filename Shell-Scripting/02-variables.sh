049ac587c798edcb118bd3edc8037d21e1f80d7a
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-05 22:15:50 +0530

    add

--- Shell-Scripting/02-variables.sh
+++ Shell-Scripting/02-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Use $(( expression )) for calculations:
 
-number1=$1
-number2=$2
+number1=10
+number2=20
 sum=$((number1 + number2))  #  addition
-echo "Sum of $number1 + $number2 is: $sum"
+echo "Sum of two numbers is: $sum"
