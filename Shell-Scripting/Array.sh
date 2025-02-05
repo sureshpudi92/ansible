c3f508932f4404b29bceb099bcae249462e956aa
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-05 22:39:00 +0530

    add

--- Shell-Scripting/Array.sh
+++ Shell-Scripting/Array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 fruit=("apple" "banana" "mango")
-echo "first fruit from array is ${fruit([0]}"
-echo "first fruit from array is ${fruit([@]}"
+echo "first fruit from array is ${fruit[0]}"
+echo "first fruit from array is ${fruit[@]}"
