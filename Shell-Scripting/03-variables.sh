205e4f46bedd27a3bc4763f360aee63420a317a7
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-05 16:49:24 +0530

    first script

--- Shell-Scripting/03-variables.sh
+++ Shell-Scripting/03-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # read command is used to to read the user input
+#use -s to not visible while entering
 
 echo "enter your username"
-read -s user_name
+read  user_name
 echo " username is $user_name"
+read  -s Password
+echo "password is $Password"
