0e49775e0239220b0d4e14f29a1f26028871340d
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-06 17:23:57 +0530

    add

--- Shell-Scripting/Install-Mysql.sh
+++ Shell-Scripting/Install-Mysql.sh
@@ -13,10 +13,10 @@ N="\e[0m"
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-        echo -e"$2 ..... $R failure $N"
+        echo -e "$2 ..... $R failure $N"
         exit 1
 else
-echo -e"$2 .....$G success $N"
+echo -e "$2 .....$G success $N"
 fi
 
 }
