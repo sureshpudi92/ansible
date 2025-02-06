55793b87dbe3ad4aea5763002f772d36991053d3
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-06 13:04:03 +0530

    add

--- Shell-Scripting/Install-Mysql.sh
+++ Shell-Scripting/Install-Mysql.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 if [ $USERID ne 0 ]
 then
-echo "Error: please run this script with root access"]
+echo "Error: please run this script with root access"
 else
 echo "you are root user"
 fi
