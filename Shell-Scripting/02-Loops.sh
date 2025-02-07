0a8c32ed49a517dbd6055ae42e75897752ad82da
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-07 23:00:34 +0530

    add

--- Shell-Scripting/02-Loops.sh
+++ Shell-Scripting/02-Loops.sh
@@ -4,3 +4,11 @@ for i in $@
 do
      yum install $i -y
 done
+
+# improvements
+# implement log files
+# implement colors
+# implement validations
+# implement validations through functions
+# your script should check package is already installed or not, if already installed print yellow color
+# if installed just print package is alredy installed, it should not run install command
