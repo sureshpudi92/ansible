ae17a28aef85a34fac3a2d4438145299e7621e42
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-10 22:58:28 +0530

    add

--- Shell-Scripting/number.sh
+++ Shell-Scripting/number.sh
@@ -3,10 +3,10 @@
 
 
 
-for i in {1..100}
+for i in {1..100};
 do
     if ([ $i % 3==0 ] || [ $i % 5==0 ]) $$ [ $i % 15 !=0 ];
     then
-        echo "Number is $i"
-    fi
+        echo  $i
+    fi;
 done
