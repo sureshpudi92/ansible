c1fdef5e8af2d3632dc2bf4b99d95ed075eb0ea0
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-10 23:23:13 +0530

    add

--- Shell-Scripting/number.sh
+++ Shell-Scripting/number.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# write a shell script to print numbers divided by 3 & 5 and not 15…..range 1…100
 
-
-
-for i in {1..100};do
-    if ([ 'expr $i % 3' == 0 ] || [ 'expr $i % 5' == 0 ]) && [ 'expr $i % 15' != 0 ];
-    then
+for i in {1..100}; 
+do
+    if (( (i % 3 == 0 || i % 5 == 0) && i % 15 != 0 )); then
         echo "$i"
-    fi;
+    fi
 done
