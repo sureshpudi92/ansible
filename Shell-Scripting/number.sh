c4ae0e34dc850d8b913a5fb9fe75ad691356ea6f
Suresh Pudi <suresh.pudi92@gmail.com.com>
2025-02-10 23:00:13 +0530

    add

--- Shell-Scripting/number.sh
+++ Shell-Scripting/number.sh
@@ -3,10 +3,11 @@
 
 
 
-for i in {1..100};
+#!/bin/bash
+
+for i in {1..100}; 
 do
-    if ([ $i % 3==0 ] || [ $i % 5==0 ]) $$ [ $i % 15 !=0 ];
-    then
-        echo  $i
-    fi;
+    if (( (i % 3 == 0 || i % 5 == 0) && i % 15 != 0 )); then
+        echo "$i"
+    fi
 done
